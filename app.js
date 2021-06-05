64109e080ce2f2526b4c116f7a7168c126f856d1
Muhammad Aadil <adilbanaras@gmail.com>
2021-06-05 15:56:13 +0400

    swager removed, postman added

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
 
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
@@ -25,8 +23,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/', indexRouter);
 app.use('/api/auth', authRouter);
